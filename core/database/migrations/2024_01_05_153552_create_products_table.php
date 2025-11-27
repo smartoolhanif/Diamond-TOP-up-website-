5276ce8ee2132b633054acc54f8e4ddd4861d282
smartoolhanif <clashofkinghanif44@gmail.com>
2025-11-27 13:13:44 +0600

    Upload 2024_01_05_153552_create_products_table.php

---
+++ core/database/migrations/2024_01_05_153552_create_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Constants\Status;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->text('title');
+            $table->string('slug')->unique();
+            $table->text('content');
+            $table->enum('type', [Status::INGAME, Status::TOPUP, Status::VOUCHER]);
+            $table->integer('percentage')->nullable()->default(0);
+            $table->integer('order_column')->default(0);
+            $table->boolean('status')->default(Status::ACTIVE);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
